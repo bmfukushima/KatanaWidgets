e40f1ace7f669ec93b0754ac254d020eea5be03b
bmfukushima <bmfukushima@gmail.com>
2020-09-14 19:09:36 -0700

    First look at node shape modifier

--- ParameterMenu/NodeShapeAttributesWidget.py
+++ ParameterMenu/NodeShapeAttributesWidget.py
@@ -1,8 +1,15 @@
 
 from Widgets2 import AbstractTabWidget
 
-from PyQt5.QtWidgets import  QWidget, QLabel
+from PyQt5.QtWidgets import  (
+    QWidget, QLabel, QHBoxLayout, QLineEdit, QPushButton
+)
 from PyQt5.QtCore import Qt
+
+try:
+    from Katana import NodegraphAPI, Utils
+except ModuleNotFoundError:
+    pass
 
 # DrawingModule.nodeWorld_setShapeAttr(node, 'badgeText', badgeText)
 """
@@ -21,28 +28,132 @@ def _RemoveGlow(node):
 """
 
 
-class NodeShapeAttrsWidget(AbstractTabWidget):
+class NodeShapeAttrsTab(AbstractTabWidget):
+    """
+    Widget that holds all of the shape attributes available for the user to
+    adjust on the node
+    """
     NodeShapeTypes = {
-        "badgeText":str,
-        "drawBadge":bool,
-        "glowColorR":float,
-        "glowColorG":float,
-        "glowColorB":float,
-        "errorGlow":bool
+        "badgeText": {"type":str, "value":""},
+        "drawBadge":{"type":bool, "value":False},
+        "glowColorR":{"type":float, "value":0.5},
+        "glowColorG":{"type":float, "value":0.5},
+        "glowColorB":{"type":float, "value":0.5},
+        "errorGlow":{"type":bool, "value":False}
     }
-    def __init__(self, parent=None):
-        super(NodeShapeAttrsWidget, self).__init__(parent)
+
+    def __init__(self, parent, node):
+        super(NodeShapeAttrsTab, self).__init__(parent)
         self.setType(AbstractTabWidget.STACKED)
         self.setTabPosition(AbstractTabWidget.WEST)
         self.setMultiSelect(True)
         self.setMultiSelectDirection(Qt.Vertical)
+        self.setNode(node)
 
-        for i, x in enumerate(NodeShapeAttrsWidget.NodeShapeTypes.keys()):
-            widget = QLabel(x)
-            self.insertTab(i, widget, x)
+        for i, shape_name in enumerate(sorted(NodeShapeAttrsTab.NodeShapeTypes.keys())):
+            default_value = NodeShapeAttrsTab.NodeShapeTypes[shape_name]['value']
+            value_type = NodeShapeAttrsTab.NodeShapeTypes[shape_name]['type']
+            widget = NodeShapeAttrsWidget(self, node, shape_name, default_value, value_type)
+            self.insertTab(i, widget, shape_name)
+            # shape name
+            # default value
         # w.setType(AbstractTabWidget.DYNAMIC, dynamic_widget=TabDynamicWidgetExample,
         #           dynamic_function=TabDynamicWidgetExample.updateGUI)
 
+    def getNode(self):
+        return self._node
+
+    def setNode(self, node):
+        self._node = node
+
+
+class NodeShapeAttrsWidget(QWidget):
+    """
+    Individual widget holding the GUI to change a shape
+    node
+    value
+    update function?
+    """
+    def __init__(self, parent, node, shape_name, default_value, value_type):
+        super(NodeShapeAttrsWidget, self).__init__(parent)
+        # set attrs
+        self._node = node
+        self._shape_name = shape_name
+        self._default_value = default_value
+        self._value_type = value_type
+
+        # create widgets
+        QHBoxLayout(self)
+        label = QLabel(shape_name)
+        self.user_input_widget = self.createUserInputWidget()
+        # edit_field = QLineEdit()
+        # edit_field.setText(str(default_value))
+        # edit_field.editingFinished.connect(self.test)
+
+        # setup layout
+        self.layout().addWidget(label)
+        self.layout().addWidget(self.user_input_widget)
+
+    def createUserInputWidget(self):
+        """
+        Creates a widget for the user to input stuff with based off of the
+        type of data provided
+        """
+        value_type = self.getValueType()
+        if value_type in [str, float]:
+            print('string')
+            widget = QLineEdit()
+            widget.setText(str(self.getDefaultValue()))
+            widget.editingFinished.connect(self.userInputEvent)
+        elif value_type == bool:
+            widget = QPushButton()
+            widget.setCheckable(True)
+            widget.clicked.connect(self.userInputEvent)
+        return widget
+
+    def userInputEvent(self):
+        value_type = self.getValueType()
+        if value_type == str:
+            value = str(self.user_input_widget.text())
+        elif value_type == float:
+            try:
+                value = float(self.user_input_widget.text())
+            except ValueError:
+                value = 0.5
+        elif value_type == bool:
+            value = self.user_input_widget.isChecked()
+            if value:
+                value = 1.0
+            else:
+                value = 0.0
+
+        NodegraphAPI.SetNodeShapeAttr(self.getNode(), self.getShapeName(), value)
+        Utils.EventModule.QueueEvent('node_setShapeAttributes', hash(self.getNode()), node=self.getNode())
+
+    """ PROPERTIES """
+    def getNode(self):
+        return self._node
+
+    def setNode(self, node):
+        self._node = node
+
+    def getShapeName(self):
+        return self._shape_name
+
+    def setShapeName(self, shape_name):
+        self._shape_name = shape_name
+
+    def getDefaultValue(self):
+        return self._default_value
+
+    def setDefaultValue(self, default_value):
+        self._default_value = default_value
+
+    def getValueType(self):
+        return self._value_type
+
+    def setValueType(self, value_type):
+        self._value_type = value_type
 
 
 if __name__ == "__main__":
@@ -51,7 +162,7 @@ if __name__ == "__main__":
     from PyQt5.QtGui import QCursor
     app = QApplication(sys.argv)
 
-    w = NodeShapeAttrsWidget()
+    w = NodeShapeAttrsTab(None, 'blahblah')
 
     # for x in range(3):
     #     nw = QLabel(str(x))
--- ParameterMenu/ParametersMenuWidgets.py
+++ ParameterMenu/ParametersMenuWidgets.py
@@ -8,7 +8,7 @@ from Utils2.settings import BEBOP_ON_JPG, BEBOP_OFF_JPG
 
 from Utils2 import getWidgetAncestor
 
-from .NodeShapeAttributesWidget import NodeShapeAttrsWidget
+from .NodeShapeAttributesWidget import NodeShapeAttrsTab
 
 
 class ParametersMenuButton(QLabel):
@@ -105,16 +105,16 @@ class ParametersMenuButton(QLabel):
 class ParametersMenu(QMenu):
     def __init__(self, parent=None):
         super(ParametersMenu, self).__init__(parent)
-        self.addAction('Node Shape Attrs', self.showNodeShapeAttrsWidget)
+        self.addAction('Node Shape Attrs', self.showNodeShapeAttrsTab)
         self.addAction('test', self.test)
         self.addAction('test', self.test)
 
-    def showNodeShapeAttrsWidget(self):
+    def showNodeShapeAttrsTab(self):
         form_widget = self.parent().parent()
 
         # create widget if it doesnt exist
         if not hasattr(form_widget, 'node_shape_attrs_widget'):
-            node_shape_attrs_widget = NodeShapeAttrsWidget(form_widget)
+            node_shape_attrs_widget = NodeShapeAttrsTab(form_widget, self.parent().getNode())
             form_widget.node_shape_attrs_widget = node_shape_attrs_widget
             form_widget.layout().insertWidget(1, node_shape_attrs_widget)
 
