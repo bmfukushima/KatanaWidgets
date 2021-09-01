e99eac9c20a582a6f716d50eda425124ad61fb82
emmuh <bmfukushima@gmail.com>
2021-09-01 08:58:57 -0700

    ParametersMenu | Fixed small bug

--- MultiTools/__init__.py
+++ MultiTools/__init__.py
@@ -1,7 +1,3 @@
-# from .VariableManager import SuperTool as VariableManager
-# from .SimpleTool import SuperTool as SimpleTool
-# from .EventsTab import Tabs
-
 import VariableManager
 import SimpleTool
 import EventsTab
--- ParameterMenu/NodeShapeAttributesWidget.py
+++ ParameterMenu/NodeShapeAttributesWidget.py
@@ -50,7 +50,7 @@ class NodeShapeAttrsTab(ShojiModelViewWidget):
 
     def __init__(self, parent, node):
         super(NodeShapeAttrsTab, self).__init__(parent)
-        self.setDelegateType(ShojiModelViewWidget.STzACKED)
+        self.setDelegateType(ShojiModelViewWidget.STACKED)
         # todo fix scope out
         # why does this scope out?
         from cgwidgets.settings import attrs
