59b2086c65e680e972e610b5866a453c84157386
emmuh <bmfukushima@gmail.com>
2021-09-30 12:28:33 -0700

    Updating script editor

--- Scripts/1101951632188788352.SceneGraph/1319257901954977536.Bookmarks.py
+++ Scripts/1101951632188788352.SceneGraph/1319257901954977536.Bookmarks.py
@@ -1,20 +1,39 @@
-"""You can access scene graph bookmarks through the ScenegraphBookmarkManager module.
+""" From Stefan Habel via Discord
+You can access scene graph bookmarks through the ScenegraphBookmarkManager module.
 
 For example, ScenegraphBookmarkManager.GetScenegraphBookmarks() provides a list of dictionaries of scene graph bookmarks, each with entries such as a bookmark's full name, and the XML description of the various Working Set states. The full name may contain names of folders in which bookmarks are nested, with / serving as a path separator, .e.g 'My Folder Name/My Bookmark Name'.
 
 For example, to print the full names of all available bookmarks:
-for bookmark in
-""" ScenegraphBookmarkManager.GetScenegraphBookmarks():
+for bookmark in ScenegraphBookmarkManager.GetScenegraphBookmarks():
     print(bookmark['fullName'])
 
-"""
 The ScenegraphBookmarkManager module provides a function named LoadBookmark() that takes such a bookmark dictionary and activates the bookmark represented by it. Unfortunately, the name of that particular function was not included in the module's __all__ variable (this looks like a bug). That's why it appears that the function doesn't exist in the ScenegraphBookmarkManager module, as that module is made available as a "virtual" Katana module which only considers names that are exposed via __all__.
 
 As a workaround, you can call the LoadBookmark() function by accessing the module directly from where it actually lives, which is  PyUtilModule.ScenegraphBookmarkManager, for example in a custom utility function like the following:
 import PyUtilModule.ScenegraphBookmarkManager
 """
+bookmark = ScenegraphBookmarkManager.GetScenegraphBookmarks()["folder/bookmark"]
+
 def ActivateBookmark(fullName):
     for bookmark in ScenegraphBookmarkManager.GetScenegraphBookmarks():
         if bookmark['fullName'] == fullName:
             PyUtilModule.ScenegraphBookmarkManager.LoadBookmark(bookmark)
             return
+
+# get all bookmarks
+""" Returns a dictionary of bookmarks with the keys
+'name', 'scenegraphExpansion', 'render', 'viewerVisibility', 'scenegraphPinning', 'liveRenderUpdates', 'fullName', 'folder'"""
+for bookmark in ScenegraphBookmarkManager.GetScenegraphBookmarks():
+    print(bookmark.keys())
+ScenegraphBookmarkManager.GetScenegraphBookmarks()
+
+
+# create bookmark
+workingSetsToSave = ["liveRenderUpdates", "render", "scenegraphExpansion", "scenegraphPinning", "scenegraphSelection", "viewerVisibility"]
+ScenegraphBookmarkManager.CreateWorkingSetsBookmark("folder/test", workingSetsToSave)
+
+# delete bookmark
+ScenegraphBookmarkManager.DeleteScenegraphBookmark("folder/bookmark")
+
+# load bookmark
+ScenegraphBookmarkManager.LoadBookmark("folder/bookmark")
