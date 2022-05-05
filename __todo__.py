29e273d0058d1c1a836bad740cbaeb646bfe92e7
emmuh <bmfukushima@gmail.com>
2022-05-05 12:02:23 -0700

    updating todo list

--- __todo__.py
+++ __todo__.py
@@ -4,10 +4,6 @@ TODO: Enhancement
     *   VariableManager | Can load multiple patterns
             Remake this?
 TODO: BUGS
-    *   Script Manager (Hotkeys)
-            Setting new hotkey does not remove old hotkey
-    *   Popup Widgets
-            Saving widget does not save icon?
     *   Node Graph
             If no nodegraphs found, will fail on init
 
