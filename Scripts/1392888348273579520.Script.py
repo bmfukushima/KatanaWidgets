c57fa83ea73ee466b89aab7bb7ebd525bb9cf05b
emmuh <bmfukushima@gmail.com>
2022-05-05 20:14:20 -0700

    Cleanup scripts

--- Scripts/1392888348273579520.Script.py
+++
@@ -1,9 +0,0 @@
-from cgwidgets.utils import getWidgetUnderCursor
-from Katana import UI4
-
-w = getWidgetUnderCursor()
-print("mouse_pos ==", w.getMousePos())
-
-# nodegraph_tab = UI4.App.Tabs.FindTopTab('Node Graph')
-# nodegraph_widget = nodegraph_tab.getNodeGraphWidget()
-# print(nodegraph_widget)
